49b2ada7fc1b76b569ed27aa1bf59501b73e1981
RubenDarioJ <rubendjusto02@gmail.com>
2024-04-08 15:08:42 -0400

    first commit

---
+++ src/views/MoneyInfo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useBankDataStore } from '@/stores/bankData'
+
+const bankDataStore = useBankDataStore()
+</script>
+
+<template>
+    <h1>Todas tus transacciones</h1>
+    <div v-for="item in bankDataStore.data">
+       <p>Usted aprobó un retiro por un total: ${{ item.monto }} | Su saldo actual es de ${{ item.saldo }}</p> 
+    </div>
+</template>
+
+<style scoped>
+
+</style>
