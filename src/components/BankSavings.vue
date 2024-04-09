d13a1782da87e0c573c73af88d3c267f416f99aa
RubenDarioJ <rubendjusto02@gmail.com>
2024-04-08 22:09:42 -0400

    update

--- src/components/BankSavings.vue
+++ src/components/BankSavings.vue
@@ -6,25 +6,31 @@ const transactionDataStore = useTransactionDataStore()
 </script>
 
 <template>
-  <h1>Su saldo actual es de: ${{ transactionDataStore.data[0].savings }}</h1>
+  <h1>
+    Su saldo actual es de:<span
+    :class="transactionDataStore.data[0].savings >= 1 ? 'positive' : ''">
+      ${{ transactionDataStore.data[0].savings }}
+    </span>
+  </h1>
   <button @click="transactionDataStore.deposit"
   :disabled="transactionDataStore.data[0].savings >= 50">Depositar</button>
 
   <button @click="transactionDataStore.withdraw"
   :disabled="transactionDataStore.data[0].savings <= 0">Retirar</button>
-<div v-for="item in transactionDataStore.data">
-  <ul>
-    <li>
-    <span v-if="item.currentAmount >= 0">
-      Usted ha depositado ${{ item.currentAmount }} | Saldo actual {{ item.balance }}  </span>
-    <span v-else>
-      Usted ha retirado ${{ item.currentAmount }} | Saldo actual {{ item.balance }} </span>
-    <RouterLink :to="`/info/${item.id}`">
-    <p>Ver mas información</p>
-   </RouterLink>
-    </li>
-  </ul>
-</div>
+
+  <div v-for="item in transactionDataStore.data">
+    <ul>
+      <li>
+      <span v-if="item.currentAmount >= 0">
+        Usted ha depositado ${{ item.currentAmount }} | Saldo actual {{ item.balance }}  </span>
+      <span v-else>
+        Usted ha retirado ${{ item.currentAmount }} | Saldo actual {{ item.balance }} </span>
+      <RouterLink :to="`/info/${item.id}`">
+      <p>Ver mas información</p>
+    </RouterLink>
+      </li>
+    </ul>
+  </div>
 
 </template>
 <style scoped>
@@ -55,6 +61,10 @@ button[disabled]{
 
 li {
   list-style-type: none;
+}
+
+.positive {
+  color: rgb(12, 187, 12);
 }
 
 </style>@/stores/transactionData
