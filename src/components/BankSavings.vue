4f4be72855560ea3688db08aed04eded7ee81b83
RubenDarioJ <rubendjusto02@gmail.com>
2024-04-08 22:23:56 -0400

    update

--- src/components/BankSavings.vue
+++ src/components/BankSavings.vue
@@ -22,9 +22,9 @@ const transactionDataStore = useTransactionDataStore()
     <ul>
       <li>
       <span v-if="item.currentAmount >= 0">
-        Usted ha depositado ${{ item.currentAmount }} | Saldo actual {{ item.balance }}  </span>
+        Usted ha depositado ${{ item.currentAmount }} | Saldo actual ${{ item.balance }}  </span>
       <span v-else>
-        Usted ha retirado ${{ item.currentAmount }} | Saldo actual {{ item.balance }} </span>
+        Usted ha retirado ${{ item.currentAmount }} | Saldo actual ${{ item.balance }} </span>
       <RouterLink :to="`/info/${item.id}`">
       <p>Ver mas información</p>
     </RouterLink>
