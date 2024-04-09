a00fdf4771f5e1e90d81cfe4740cfe8e1e72e5a6
RubenDarioJ <rubendjusto02@gmail.com>
2024-04-08 22:31:01 -0400

    update

---
+++ src/views/TransactionInfo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useTransactionDataStore } from '@/stores/transactionData'
+
+const transactionDataStore = useTransactionDataStore()
+</script>
+
+<template>
+    <h1>Informacion de esta transacción {{ transactionDataStore.data[0].currentAmount }}</h1>
+    <div v-for="item in transactionDataStore.data">
+       <p>Usted aprobó un retiro por un total: ${{ item.currentAmount }} | Su saldo actual es de ${{ item.balance }}</p> 
+    </div>
+</template>
+
+<style scoped>
+
+</style>@/stores/transactionData
